622da16725e35d5d7e95436e5c4da6fc8bad5a47
Suparna Ghanvatkar <suparna-ghanvatkar@users.noreply.github.com>
2016-04-03 17:51:49 +0530

    Severity n topic model

---
+++ topicsev.R
@@ -0,0 +1,46 @@
+library(lda)
+library(ggplot2)
+library(reshape2)
+library(tm)
+library(SnowballC)
+library(RMySQL)
+library(stringr)
+drv=dbDriver("MySQL")
+con=dbConnect(drv,dbname='bug_report',user='root')
+stat="select Bugzilla_severity.report_id,Bugzilla_severity.timestamp,Bugzilla_severity.what as 'severity',Bugzilla_desc.what as 'desc' from Bugzilla_severity left join Bugzilla_desc on (Bugzilla_desc.report_id=Bugzilla_severity.report_id and Bugzilla_desc.timestamp=Bugzilla_severity.timestamp);"
+table1=dbGetQuery(con,stat)
+sw <- c(stopwords("english"),"na","NA","content","will")
+desc<-table1["desc"]
+docu=""
+i<-1
+while(i<=5616)
+{
+  temp<-desc[i,1]
+  temp<-lapply(temp,tolower)
+  temp<-lapply(temp,removePunctuation)
+  temp<-lapply(temp,removeNumbers)
+  temp<-unlist(temp)
+  temp<-removeWords(temp,sw)
+  temp<-strsplit(temp," ")
+  temp<-lapply(temp,stemDocument)
+  j<-2
+  while(j<=length(temp[[1]]))
+  {
+    temp[[1]][1]<-paste(temp[[1]][1],temp[[1]][j])
+    j<-j+1
+  }
+  temp<-temp[[1]][1]
+  desc[i,1]<-relist(temp,desc[i,1])
+  docu[[i]]<-desc[i,1]
+  i<-i+1
+}
+docs<-lexicalize(docu)
+vocab=docs$vocab
+docs=docs$documents
+K<-10
+result<-lda.collapsed.gibbs.sampler(docs,K,vocab,800,0.15,0.01,compute.log.likelihood=TRUE)
+top.words <- top.topic.words(result$topics, 20, by.score=TRUE)
+table1['desc']<-docu
+View(result$document_sums)
+View(subset(table1,desc!="NA"))
+
